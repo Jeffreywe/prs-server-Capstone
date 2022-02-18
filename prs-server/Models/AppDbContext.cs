7a2244dde3a7f4bfdf9b1739b4ec6fdc0aad4fa0
Jeffrey Evans <jeffreyevans.qcec@aol.com>
2022-02-18 11:37:55 -0500

    Add project files.

---
+++ prs-server/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prs_server.Models {
+    public class AppDbContext : DbContext {
+
+        public virtual DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+
+        }
+    }
+}
